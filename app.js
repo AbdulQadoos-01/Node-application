3dd7000542a21ba609ede37c4ea9ecef13b048fc
AbdulQadoos-01 <127832116+AbdulQadoos-01@users.noreply.github.com>
2023-08-21 10:35:50 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, this is a sample Node.js application!\n');
+  res.end('Hello, this is a sample Node.js app!\n');
 });
 
 const PORT = process.env.PORT || 3008;
